3002cbf49698f52395ff59064c78e88d098b8275
Lunar-Academy <69018644+Lunar-Academy@users.noreply.github.com>
2025-02-24 11:16:05 -0500

    added photonvision subsystem

---
+++ src/main/java/frc/robot/subsystems/PhotonVision/Photon.java
@@ -0,0 +1,48 @@
+package frc.robot.subsystems.PhotonVision;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
+import java.util.List;
+import org.littletonrobotics.junction.Logger;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+public class Photon extends SubsystemBase {
+  private PhotonIO io;
+
+  private final PhotonIOInputsAutoLogged inputs = new PhotonIOInputsAutoLogged();
+
+  private PhotonCamera camera;
+
+  public Photon(PhotonIO io) {
+    io = this.io;
+    if (inputs.name != "sim") {
+      camera = new PhotonCamera(inputs.name);
+    }
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Photon" + inputs.name, inputs);
+  }
+
+  public List<PhotonPipelineResult> result() {
+    if (inputs.name != "sim") {
+      return camera.getAllUnreadResults();
+    }
+    return new ArrayList<PhotonPipelineResult>();
+  }
+
+  public boolean hasTarget() {
+    return inputs.hasTargets;
+  }
+
+  public boolean hasCoral() {
+    return inputs.hasCoral;
+  }
+
+  public boolean hasAlgae() {
+    return inputs.hasAlgae;
+  }
+}
