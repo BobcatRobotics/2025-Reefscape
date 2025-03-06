c9fd1392de3e9f72f05d37acf8292765fa49a829
Arnav Thakrar <arnav.thakrar@gmail.com>
2025-03-05 19:15:01 -0500

    code

--- src/main/java/frc/robot/subsystems/Superstructure/Elevator/ElevatorState.java
+++ src/main/java/frc/robot/subsystems/Superstructure/Elevator/ElevatorState.java
@@ -20,7 +20,7 @@ public enum ElevatorState {
   CORAL_L4(Rotation2d.fromRotations(3.1)),
   POST_CORAL_L4(Rotation2d.fromRotations(2.3)),
   INTAKE_SAFE_ZONE(Rotation2d.fromRotations(1.75)), // safe height for flipping up the arm
-  ALGAE_L2(Rotation2d.fromRotations(1.5)),
+  ALGAE_L2(Rotation2d.fromRotations(1.25)),
   ALGAE_L3(Rotation2d.fromRotations(2.35)),
   ALGAE_GROUND(Rotation2d.fromRotations(0.2)), // pickup algae from ground
   ALGAE_SCORE_PROCESSOR(Rotation2d.fromRotations(0)), // pickup algae from processor
