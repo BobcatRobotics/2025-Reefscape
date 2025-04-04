69964e76b651f44514dffccf467b9d0c0c0f3e8e
Arnav Thakrar <arnav.thakrar@gmail.com>
2025-04-04 08:56:38 -0400

    Update at 'Fri Apr 04 08:56:38 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -318,6 +318,10 @@ public class RobotContainer {
                         },
                         intake)
                     .until(intake::hasPiece)));
+    
+    NamedCommands.registerCommand("IntakeDrop", Commands.run(()->intake.deploy()));
+
+    
 
     NamedCommands.registerCommand(
         "HandoffThenPrep",
@@ -325,7 +329,7 @@ public class RobotContainer {
             new WaitCommand(.5).until(() -> (intake.frontSensor())),
             new WaitCommand(0.35)
                 .until(() -> intake.hasPiece() && !intake.frontSensor())
-                .andThen(new WaitCommand(0.1))
+                .andThen(new WaitCommand(0.0))
                 .deadlineFor(Commands.run(() -> intake.retract())),
             SuperstructureActions.handoffThenPrepL4Auto(superstructure, endEffector)));
 
